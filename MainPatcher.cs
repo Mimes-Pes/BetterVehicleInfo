6d9f7f4a26910e8da3b627562b9a36353fc928e0
Mimes Pes <77901237+Mimes-Pes@users.noreply.github.com>
2021-01-24 08:59:29 +1030

    BetterVehicleInfo
    BetterVehicleInfo

---
+++ MainPatcher.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Reflection;
+using BetterVehicleInfo.Modules;
+using Harmony;
+using SMLHelper.V2.Handlers;
+
+// You can use this file almost as-is. Just change the marked lines below. This will be the main file of each mod that tells Harmony to load your changes.
+namespace BetterVehicleInfo     // Change this line to match your mod.
+{
+    public class MainPatcher
+    {
+        private static Assembly thisAssembly = Assembly.GetExecutingAssembly();
+        private static string ModPath = Path.GetDirectoryName(thisAssembly.Location);
+        internal static string AssetsFolder = Path.Combine(ModPath, "Assets");
+
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.mimes.subnautica.bettervehicleinfo");   // Change this line to match your mod.
+            var betterVehicleInfo = new BetterVehicleInfoModule();
+            betterVehicleInfo.Patch();
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Config.Load();
+            OptionsPanelHandler.RegisterModOptions(new Options());
+        }
+    }
+}
+
+/*
+// Main file of the mod passing info to Harmony to load these changes.
+namespace MoonpoolDisplayRepair     // Name of the mod.
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.mimes.subnautica.moonpooldisplayrepair");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Config.Load();
+            OptionsPanelHandler.RegisterModOptions(new Options());
+        }
+
+    }
+}
+*/
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BetterVehicleInfo
- BetterVehicleInfo
+It is a Seamoth piloting upgrade mod, the upgrade is unlocked with Base Upgrade Console. Making this upgrade requires
+- Computer Chip,
+- Advanced Wiring Kit and
+- Gold.
+It is a mod which improves dashboard and adds gears to Seamoth:
+- Gear 1 = 25% speed,
+- 2 = 50% speed,
+- 3 = 75% speed,
+- 4 = vanilla speed,
+- 5 = 125% speed,
+- 6 = 150% speed.
+Reasoning behind slow speed is to facilitate nice approaches to bases etc.
+Mod also adjusts energy usage depending on speed 1 = lowest usage than vanilla, 5 and 6 = exponentially higher usage - very high resistance at high speed. Extra gear state (1-6) info is implemented, and it shows bottom RHS just above Health, Charge and Temp panel. It shows which gear is Seamoth currently in. Mod has extensive customisation options.
